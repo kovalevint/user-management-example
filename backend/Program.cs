a58f35512d11aa6e22e60fc1ab28e75c0b9f63e8
kovalevint <ihor.kovalov@intellias.com>
2024-09-23 13:45:43 +0200

    Change docker stuff

--- backend/Program.cs
+++ backend/Program.cs
@@ -16,6 +16,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () => "Hello World!");
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
